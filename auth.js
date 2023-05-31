723b3f267c4849e229cb15ee4b7b05a80730b69c
Matthew-Salut08 <122797143+Matthew-Salut08@users.noreply.github.com>
2023-05-31 10:46:31 +0800

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,45 +1,21 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import jwt from "jsonwebtoken";
 
-export const register = async (req, res) => {
+export const verifyToken = async (req, res, next) => {
     try {
-        const {firstname, lastname, email, password} = req.body;
-        const salt = await bcrypt.genSalt();
-        const encryptedPassword = await bcrypt.hash(password, salt);
-        const newUser = await User.create({
-            firstname,
-            lastname,
-            email,
-            password: encryptedPassword
-        })
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
+        let token = req.header("Authorization");
+
+        if(!token) {
+            return res.status(401).send("Unauthorized");
+        }
+
+        if(token.startsWith("Bearer ")) {
+            token = token.slice(7, token.length).trimLeft();
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
     } catch (err) {
         res.status(500).json({error: err.message});
-    }
-}
-
-export const login = async (req, res) => {
-    try {
-        const{email, password} = req.body;
-
-        const user = await User.findOne({
-            email: email
-        });
-
-        if(!user) return res.status(400).json({msg: 'Invalid Email/Password'});
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-
-        if(isPasswordValid) {
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
-            user.password = '***';
-            res.status(200).json({token, user});
-        } else {
-            res.status(400).json({msg: 'Invalid Credentials'})
-        }
-    } catch(err) {
-        res.status(500).json({error: err.message})
     }
 }
